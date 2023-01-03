e1cd98bb2959b0f321c2e1f39be66b7f86935c30
lamongabriel <gabriel-lamon@outlook.com>
2023-01-03 14:16:40 -0300

    Users listing

---
+++ src/pages/users/index.tsx
@@ -0,0 +1,67 @@
+import {
+	Box,
+	Button,
+	Checkbox,
+	Flex,
+	Heading,
+	Icon,
+	Table,
+	Tbody,
+	Td,
+	Th,
+	Thead,
+	Tr,
+	Text
+} from '@chakra-ui/react'
+
+import { Layout } from '../../components/Layout'
+
+import { RiAddLine } from 'react-icons/ri'
+
+export default function UserList () {
+	return (
+		<Layout>
+			<Box flex={1} borderRadius={8} bg='gray.800' p={8}>
+				<Flex mb={8} justify='space-between' align='center'>
+					<Heading size='lg' fontWeight='normal'>
+						Users
+					</Heading>
+					<Button as='a' size='sm' fontSize='sm' colorScheme='pink' leftIcon={<Icon fontSize={20} as={RiAddLine} />}>
+						New
+					</Button>
+				</Flex>
+				<Table colorScheme='whiteAlpha'>
+					<Thead>
+						<Tr>
+							<Th px={6} color='gray.300' width={8}>
+								<Checkbox colorScheme='pink'/>
+							</Th>
+							<Th>
+								USER
+							</Th>
+							<Th>
+								JOIN DATE
+							</Th>
+						</Tr>
+					</Thead>
+					<Tbody>
+						<Tr>
+							<Td px={6}>
+								<Checkbox colorScheme='pink'/>
+							</Td>
+							<Td>
+								<Box>
+									<Text fontWeight='bold'>Gabriel Lamon</Text>
+									<Text fontSize='small' color='gray.300'>gabriel-lamon@outlook.com</Text>
+								</Box>
+							</Td>
+							<Td>
+								May 11, 2013
+							</Td>
+						</Tr>
+					</Tbody>
+				</Table>
+			</Box>
+		</Layout>
+	)
+}
