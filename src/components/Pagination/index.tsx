f56becac080e65f0926a596a88fd0f9ba882d928
lamongabriel <gabriel-lamon@outlook.com>
2023-01-03 15:59:43 -0300

    pagination - componentization

---
+++ src/components/Pagination/index.tsx
@@ -0,0 +1,25 @@
+import { Stack, Box } from '@chakra-ui/react'
+
+import { PaginationItem } from './PaginationItem'
+
+export function Pagination () {
+	return (
+		<Stack
+			spacing={6}
+			direction='row'
+			mt={8}
+			justifyContent='space-between'
+			align='center'
+		>
+			<Box>
+				<strong>0</strong> - <strong>10</strong> of <strong>100</strong>
+			</Box>
+			<Stack direction='row' spacing={2}>
+				<PaginationItem pageNumber={1} isCurrent/>
+				<PaginationItem pageNumber={2}/>
+				<PaginationItem pageNumber={3}/>
+				<PaginationItem pageNumber={4}/>
+			</Stack>
+		</Stack>
+	)
+}
