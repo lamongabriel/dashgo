d4aca0ac037343e724e73301cc3d561a18b7b019
lamongabriel <gabriel-lamon@outlook.com>
2023-01-03 10:15:21 -0300

    chakra theme colors

--- src/pages/_app.tsx
+++ src/pages/_app.tsx
@@ -4,7 +4,7 @@ import { ChakraProvider } from '@chakra-ui/react'
 import { theme } from '../styles/theme'
 
 export default function App({ Component, pageProps }: AppProps) {
-  return (
+	return (
 		<ChakraProvider theme={theme}>
 			<Component {...pageProps} />
 		</ChakraProvider>
--- src/styles/theme.tsx
+++ src/styles/theme.tsx
@@ -1,6 +1,20 @@
-import { extendTheme } from '@chakra-ui/react'
+import { extendTheme, ThemeOverride } from '@chakra-ui/react'
 
-export const theme = extendTheme({
+const base: ThemeOverride = {
+	colors: {
+		gray: {
+			'900': '#181b23',
+			'800': '#1f2029',
+			'700': '#353646',
+			'600': '#4b4d63',
+			'500': '#616480',
+			'400': '#797D9A',
+			'300': '#9699b0',
+			'200': '#b3b5c6',
+			'100': '#d1d2dc',
+			'50': '#eeeef2',
+		}
+	},
 	styles: {
 		global: () => ({
 			body: {
@@ -9,4 +23,6 @@ export const theme = extendTheme({
 			}
 		})
 	}
-})
+}
+
+export const theme = extendTheme(base)
