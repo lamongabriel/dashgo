3f96a6b6d3433587bfb1e8acd07f996aa2f0a185
lamongabriel <gabriel-lamon@outlook.com>
2023-01-04 13:54:45 -0300

    fade reveal

--- src/components/Layout.tsx
+++ src/components/Layout.tsx
@@ -1,6 +1,8 @@
 import { ReactNode } from 'react'
 import { Flex } from '@chakra-ui/react'
 
+import { Fade } from 'react-awesome-reveal'
+
 import { Header } from './Header'
 import { Sidebar } from './Sidebar'
 
@@ -11,11 +13,13 @@ interface LayoutProps {
 export function Layout ({children}: LayoutProps) {
 	return (
 		<Flex flexDir='column' minHeight='100vh' w='100vw' overflowX='hidden' maxWidth="1480px" mx='auto'>
-			<Header />
-			<Flex w='100%' my={6} px={6}>
-				<Sidebar />
-				{children}
-			</Flex>
+			<Fade>
+				<Header />
+				<Flex w='100%' my={6} px={6}>
+					<Sidebar />
+					{children}
+				</Flex>
+			</Fade>
 		</Flex>
 	)
 }
