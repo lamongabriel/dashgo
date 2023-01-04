8fe1b08683dc90346710528b977ff5cab1f97a09
lamongabriel <gabriel-lamon@outlook.com>
2023-01-04 10:29:11 -0300

    active link

--- src/components/Sidebar/NavLink.tsx
+++ src/components/Sidebar/NavLink.tsx
@@ -1,6 +1,7 @@
 import { Icon, Link, Text, LinkProps } from '@chakra-ui/react'
 
 import NextLink from 'next/link'
+import { useRouter } from 'next/router'
 
 import { IconType } from 'react-icons'
 interface NavLinkProps extends LinkProps{
@@ -10,8 +11,26 @@ interface NavLinkProps extends LinkProps{
 }
 
 export function NavLink ({ icon, children, href, ...rest }: NavLinkProps) {
+
+	const { asPath } = useRouter()
+
+	function checkActiveLink(path: string, href: string) {
+		if(path === '/'){
+			return href === '/'
+		}
+
+		return path.startsWith(href)
+	}
+
 	return (
-		<Link as={NextLink} href={href} display='flex' alignItems='center' {...rest}>
+		<Link
+			as={NextLink}
+			href={href}
+			color={checkActiveLink(asPath, href) ? 'pink.400' : 'inherit'}
+			display='flex'
+			alignItems='center'
+			{...rest}
+		>
 			<Icon as={icon} fontSize={20} />
 			<Text fontWeight='medium' ml={4}>{children}</Text>
 		</Link>
