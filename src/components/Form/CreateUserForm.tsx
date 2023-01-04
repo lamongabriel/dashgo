035ea3e9624b226cfee4357ddacb1e417b79a563
lamongabriel <gabriel-lamon@outlook.com>
2023-01-04 12:28:03 -0300

    create user  validation - yup and react-hook-form

---
+++ src/components/Form/CreateUserForm.tsx
@@ -0,0 +1,113 @@
+import { SimpleGrid, Flex, Button, Box, Divider, Heading } from '@chakra-ui/react'
+import Link from 'next/link'
+import { useForm, SubmitHandler } from 'react-hook-form'
+
+import * as yup from 'yup'
+import { yupResolver } from '@hookform/resolvers/yup'
+
+import { Input } from './Input'
+
+interface FormData {
+	fullName: string
+	email: string
+	password: string
+	passwordConfirmation: string
+}
+
+const createUserFormSchema = yup.object().shape({
+	fullName: yup.string().required('Full name is required.'),
+	email: yup.string().required('E-mail is required.').email('Must be a valid E-mail.'),
+	password: yup.string().required('Password is required.').min(6, 'Must be at least 6 characters.'),
+	passwordConfirmation: yup.string().oneOf([yup.ref('password'), null], 'Passwords must match')
+})
+
+export function CreateUserForm() {
+
+	const { register, handleSubmit, formState } = useForm<FormData>({
+		resolver: yupResolver(createUserFormSchema)
+	})
+
+	const onSubmit: SubmitHandler<FormData> = async (data) => {
+		await new Promise(resolve => setTimeout(resolve, 2000))
+		console.log(data)
+	}
+
+	return (
+		<Box flex={1} borderRadius={8} bg='gray.800' p={[6, 8]}>
+
+			<Heading size='lg' fontWeight='normal'>
+				Create new user
+			</Heading>
+
+			<Divider my={6} borderColor='gray.700' />
+
+			<Box as='form' onSubmit={handleSubmit(onSubmit)}>
+				<SimpleGrid minChildWidth='220px' alignItems='start' justifyContent='between' spacing={[6, 8]}>
+					<Input
+						id='fullName'
+						type='text'
+						label='Full name'
+
+						title='Full name'
+						autoComplete='name'
+
+						errors={formState.errors.fullName}
+						{...register('fullName')}
+					/>
+					<Input
+						id='email'
+						type='email'
+						label='E-mail'
+
+						title='E-mail'
+						autoComplete='email'
+
+						errors={formState.errors.email}
+						{...register('email')}
+					/>
+				</SimpleGrid>
+				<SimpleGrid minChildWidth='220px' alignItems='start' justifyContent='between' spacing={[6, 8]} mt={6}>
+					<Input
+						id='password'
+						type='password'
+						label='Password'
+
+						title='Password'
+						autoComplete='new-password'
+
+						errors={formState.errors.password}
+						{...register('password')}
+					/>
+					<Input
+						id='passwordConfirmation'
+						type='password'
+						label='Password Confirmation'
+
+						title='Password Confirmation'
+						autoComplete='new-password'
+
+						errors={formState.errors.passwordConfirmation}
+						{...register('passwordConfirmation')}
+					/>
+				</SimpleGrid>
+				<Flex gap={4} align='center' justify='flex-end' mt={8}>
+					<Button
+						as={Link}
+						href='/users'
+						type='button'
+						colorScheme='whiteAlpha'
+					>
+						Cancel
+					</Button>
+					<Button
+						colorScheme='pink'
+						type='submit'
+						isLoading={formState.isSubmitting}
+					>
+						Save
+					</Button>
+				</Flex>
+			</Box>
+		</Box>
+	)
+}
--- src/pages/users/create.tsx
+++ src/pages/users/create.tsx
@@ -1,87 +1,11 @@
-import {
-	Box,
-	Button,
-	Divider,
-	Flex,
-	Heading,
-	SimpleGrid,
-} from '@chakra-ui/react'
-import Link from 'next/link'
-import { Input } from '../../components/Form/Input'
+import { CreateUserForm } from '../../components/Form/CreateUserForm'
 
 import { Layout } from '../../components/Layout'
 
 export default function UserList () {
 	return (
 		<Layout>
-			<Box flex={1} borderRadius={8} bg='gray.800' p={[6, 8]}>
-				<Heading size='lg' fontWeight='normal'>
-					Create new user
-				</Heading>
-				<Divider my={6} borderColor='gray.700' />
-				<Box as='form'>
-					<SimpleGrid minChildWidth='220px' alignItems='center' justifyContent='between' spacing={[6, 8]}>
-						<Input
-							id='name'
-							name='name'
-							type='text'
-							label='Full name'
-							isRequired
-
-							title='Full name'
-							autoComplete='name'
-						/>
-						<Input
-							id='email'
-							name='email'
-							type='email'
-							label='E-mail'
-							isRequired
-
-							title='E-mail'
-							autoComplete='email'
-						/>
-					</SimpleGrid>
-					<SimpleGrid minChildWidth='220px' alignItems='center' justifyContent='between' spacing={[6, 8]} mt={6}>
-						<Input
-							id='password'
-							name='password'
-							type='password'
-							label='Password'
-							isRequired
-
-							title='Password'
-							autoComplete='new-password'
-						/>
-						<Input
-							id='passwordConfirmation'
-							name='passwordConfirmation'
-							type='password'
-							label='Password Confirmation'
-							isRequired
-
-							title='Password Confirmation'
-							autoComplete='new-password'
-						/>
-					</SimpleGrid>
-					<Flex gap={4} align='center' justify='flex-end' mt={8}>
-						<Button
-							as={Link}
-							href='/users'
-							type='button'
-							colorScheme='whiteAlpha'
-						>
-							Cancel
-						</Button>
-						<Button
-							colorScheme='pink'
-							type='submit'
-						>
-							Save
-						</Button>
-					</Flex>
-				</Box>
-			</Box>
+			<CreateUserForm />
 		</Layout>
 	)
 }
