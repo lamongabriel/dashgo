b9f9cee0f355cbf84fe25410a0c74bdb3aebb1d7
lamongabriel <gabriel-lamon@outlook.com>
2023-01-13 14:13:38 -0300

    users listing

--- src/pages/users/index.tsx
+++ src/pages/users/index.tsx
@@ -23,14 +23,22 @@ import { Layout } from '../../components/Layout'
 import { Pagination } from '../../components/Pagination'
 
 import { RiAddLine } from 'react-icons/ri'
+import { User } from '../../types/user'
 
-export default function UserList () {
+export default function UserList() {
 
 	const { data, isLoading, error } = useQuery('users', async () => {
 		const response = await fetch('http://localhost:3000/api/users')
 		const data = await response.json()
 
-		return data
+		const users = data.users.map((user: User) => (
+			{
+				...user,
+				createdAt: new Date(user.createdAt).toLocaleDateString('en-US', {day: '2-digit', month: 'long', year: 'numeric'})
+			}
+		))
+
+		return users.length > 0 ? users : []
 	})
 
 	const isWideVersion = useBreakpointValue({
@@ -62,7 +70,7 @@ export default function UserList () {
 						New
 					</Button>
 				</Flex>
-				{	isLoading ? (
+				{isLoading ? (
 					<Flex justify='center'>
 						<Spinner />
 					</Flex>
@@ -76,31 +84,33 @@ export default function UserList () {
 							<Thead>
 								<Tr>
 									<Th px={6} color='gray.300' width={8}>
-										<Checkbox colorScheme='pink'/>
+										<Checkbox colorScheme='pink' />
 									</Th>
 									<Th>
-								USER
+										USER
 									</Th>
 									<Th>
-								JOIN DATE
+										JOIN DATE
 									</Th>
 								</Tr>
 							</Thead>
 							<Tbody>
-								<Tr>
-									<Td px={6}>
-										<Checkbox colorScheme='pink'/>
-									</Td>
-									<Td>
-										<Box>
-											<Text fontWeight='bold'>Gabriel Lamon</Text>
-											<Text fontSize='small' color='gray.300'>gabriel-lamon@outlook.com</Text>
-										</Box>
-									</Td>
-									<Td>
-								May 11, 2013
-									</Td>
-								</Tr>
+								{data.map((user: User, index: number) => (
+									<Tr key={index}>
+										<Td px={6}>
+											<Checkbox colorScheme='pink' />
+										</Td>
+										<Td>
+											<Box>
+												<Text fontWeight='bold'>{user.name}</Text>
+												<Text fontSize='small' color='gray.300'>{user.email}</Text>
+											</Box>
+										</Td>
+										<Td>
+											{user.createdAt}
+										</Td>
+									</Tr>
+								))}
 							</Tbody>
 						</Table>
 						<Pagination />
