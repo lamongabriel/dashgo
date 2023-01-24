3e36590c07ec0154b77ce8c53944d5ab4d9560fd
lamongabriel <gabriel-lamon@outlook.com>
2023-01-23 21:01:09 -0300

    api interceptor for refreshing token

--- web/src/services/api.ts
+++ web/src/services/api.ts
@@ -1,11 +1,37 @@
-import axios from 'axios'
-import {parseCookies} from 'nookies'
+import axios, { AxiosError } from 'axios'
+import { parseCookies, setCookie } from 'nookies'
 
-const cookies = parseCookies()
+let cookies = parseCookies()
 
 export const api = axios.create({
 	baseURL: process.env.NEXT_PUBLIC_API_URL as string,
 	headers: {
 		Authorization: `Bearer ${cookies['dashgo@token']}`
+	}
+})
+
+api.interceptors.response.use(response => response, (error: AxiosError) => {
+	if(error.response?.status === 401){
+		if(error.response.data?.code === 'token.expired'){
+			// refresh token
+			cookies = parseCookies()
+			const {'dashgo@refreshToken': refreshToken} = cookies
+
+			api.post('/refresh', refreshToken).then(response => {
+				setCookie(undefined, 'dashgo@token', response.data.token, {
+					maxAge: 60 * 60 * 24, // 24 hours
+					path: '/'
+				})
+
+				setCookie(undefined, 'dashgo@refreshToken', response.data.refreshToken, {
+					maxAge: 60 * 60 * 24, // 24 hours
+					path: '/'
+				})
+
+				api.defaults.headers['Authorization'] = `Bearer ${response.data.token}`
+			})
+		} else {
+			// sign out
+		}
 	}
 })
