453e0a5b144bc90d14ba60e024dd7b59f19945bb
Gabriel Lamon <gabriel-lamon@outlook.com>
2023-02-01 19:46:07 -0300

    browser api client

--- web/src/services/api.ts
+++ web/src/services/api.ts
@@ -2,75 +2,81 @@ import axios, { AxiosError } from 'axios'
 import { parseCookies, setCookie } from 'nookies'
 import { signOut } from '../contexts/AuthContext'
 
-let cookies = parseCookies()
 let isRefreshing = false
 let failedRequestQueue: any[] = []
 
-export const api = axios.create({
-	baseURL: process.env.NEXT_PUBLIC_API_URL as string,
-	headers: {
-		Authorization: `Bearer ${cookies['dashgo@token']}`
-	}
-})
+export function setupAPIClient (ctx = undefined) {
 
-api.interceptors.response.use(response => response, (error: AxiosError) => {
-	if(error.response?.status === 401){
-		if(error.response.data?.code === 'token.expired'){
-			// refresh token
-			cookies = parseCookies()
-			const {'dashgo@refreshToken': refreshToken} = cookies
+	let cookies = parseCookies(ctx)
 
-			const originalConfig = error.config
+	const api = axios.create({
+		baseURL: process.env.NEXT_PUBLIC_API_URL as string,
+		headers: {
+			Authorization: `Bearer ${cookies['dashgo@token']}`
+		}
+	})
 
-			if(!isRefreshing){
-				isRefreshing = true
+	api.interceptors.response.use(response => response, (error: AxiosError) => {
+		if(error.response?.status === 401){
+			if(error.response.data?.code === 'token.expired'){
+				// refresh token
+				cookies = parseCookies(ctx)
+				const {'dashgo@refreshToken': refreshToken} = cookies
 
-				api.post('/refresh', {refreshToken}).then(response => {
-					setCookie(undefined, 'dashgo@token', response.data.token, {
-						maxAge: 60 * 60 * 24, // 24 hours
-						path: '/'
+				const originalConfig = error.config
+
+				if(!isRefreshing){
+					isRefreshing = true
+
+					api.post('/refresh', {refreshToken}).then(response => {
+						setCookie(ctx, 'dashgo@token', response.data.token, {
+							maxAge: 60 * 60 * 24, // 24 hours
+							path: '/'
+						})
+
+						setCookie(ctx, 'dashgo@refreshToken', response.data.refreshToken, {
+							maxAge: 60 * 60 * 24, // 24 hours
+							path: '/'
+						})
+
+						api.defaults.headers['Authorization'] = `Bearer ${response.data.token}`
+
+						failedRequestQueue.forEach(request => request.onSuccess(response.data.token))
+
+						failedRequestQueue = []
+					}).catch((err) => {
+						failedRequestQueue.forEach(request => request.onSuccess(err))
+						failedRequestQueue = []
+
+						if(typeof window !== 'undefined') {
+							signOut()
+						}
+					}).finally(() => {
+						isRefreshing = false
 					})
+				}
 
-					setCookie(undefined, 'dashgo@refreshToken', response.data.refreshToken, {
-						maxAge: 60 * 60 * 24, // 24 hours
-						path: '/'
+				return new Promise((resolve, reject) => {
+					failedRequestQueue.push({
+						onSuccess: (token: string) => {
+							originalConfig.headers['Authorization'] = `Bearer ${token }`
+
+							resolve(api(originalConfig))
+						},
+						onError: (err: AxiosError) => {
+							reject(err)
+						}
 					})
-
-					api.defaults.headers['Authorization'] = `Bearer ${response.data.token}`
-
-					failedRequestQueue.forEach(request => request.onSuccess(response.data.token))
-
-					failedRequestQueue = []
-				}).catch((err) => {
-					failedRequestQueue.forEach(request => request.onSuccess(err))
-					failedRequestQueue = []
-
-					if(typeof window !== 'undefined') {
-						signOut()
-					}
-				}).finally(() => {
-					isRefreshing = false
 				})
-			}
-
-			return new Promise((resolve, reject) => {
-				failedRequestQueue.push({
-					onSuccess: (token: string) => {
-						originalConfig.headers['Authorization'] = `Bearer ${token }`
-
-						resolve(api(originalConfig))
-					},
-					onError: (err: AxiosError) => {
-						reject(err)
-					}
-				})
-			})
-		} else {
-			if(typeof window !== 'undefined') {
-				signOut()
+			} else {
+				if(typeof window !== 'undefined') {
+					signOut()
+				}
 			}
 		}
-	}
 
-	return Promise.reject(error)
-})
+		return Promise.reject(error)
+	})
+
+	return api
+}
